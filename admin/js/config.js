78050bce56228c4349908c930f0bda45485e2d5e
Deeenchick <48181045+Deeenchick@users.noreply.github.com>
2026-07-27 17:14:54 +0300

    Create config.js

---
+++ admin/js/config.js
@@ -0,0 +1,7 @@
+// ============================================================
+// 🔑 НАСТРОЙКИ SUPABASE
+// ============================================================
+const CONFIG = {
+    SUPABASE_URL: 'https://obmhexrwltdtldjdeuxo.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_HX4idr3fjs1BvNTwZidseg_oiQddbPX'
+};
